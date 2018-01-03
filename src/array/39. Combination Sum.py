99053b9b2c44d43bf949ff3c69aa44abc137393c
Guanzhou Song <guanzhou.song@gmail.com>
2018-01-03 17:57:01 +0800

    new

--- src/array/39. Combination Sum.py
+++ src/array/39. Combination Sum.py
@@ -1,7 +1,25 @@
+import time
 class Solution(object):
   def combinationSum(self, candidates, target):
-    """
-    :type candidates: List[int]
-    :type target: int
-    :rtype: List[List[int]]
-    """
+    res = []
+    candidates.sort()
+    self.dfs(candidates, target, 0, [], res)
+    return res
+
+  def dfs(self, nums, target, index, path, res):
+    if target < 0:
+      return  # backtracking
+    if target == 0:
+      res.append(path)
+      return
+    for i in range(index, len(nums)):
+      if nums[i] > target:
+        break
+      self.dfs(nums, target - nums[i], i, path + [nums[i]], res)
+
+
+solution = Solution()
+time_start = time.time()
+print(solution.combinationSum(candidates=[2, 5, 6, 8], target=8))
+time_end = time.time()
+print(time_end - time_start)
